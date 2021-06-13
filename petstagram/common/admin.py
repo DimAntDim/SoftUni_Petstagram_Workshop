dae7edb0e5978652a75ab77b786400303c5b84d4
Dimitar Dimitrov <66394357+DimAntDim@users.noreply.github.com>
2021-06-13 08:30:48 -0500

    ''

--- petstagram/common/admin.py
+++ petstagram/common/admin.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 from django.contrib import admin
 
 # Register your models here.
-=======
-from django.contrib import admin
-
-# Register your models here.
->>>>>>> 4d8f13a8a1cea7a8cad054cea8ab082a5f7bcae3
