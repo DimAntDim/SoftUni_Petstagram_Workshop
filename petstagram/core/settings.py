3da50aa67eb8f373c4e41ea119876d332cef29b3
Dimitar Dimitrov <66394357+DimAntDim@users.noreply.github.com>
2021-07-19 20:11:10 -0500

    Add UserModel

--- petstagram/core/settings.py
+++ petstagram/core/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
     'common',
     'pets',
 ]
@@ -130,5 +131,10 @@ STATICFILES_DIRS = [
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
