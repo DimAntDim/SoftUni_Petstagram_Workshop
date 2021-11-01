b14ae0ae88c8643c07cc36ac16869aa6256379f5
Dimitar Dimitrov <66394357+DimAntDim@users.noreply.github.com>
2021-11-01 23:35:20 +0200

    try to fix cloudinary upload

--- petstagram/pets/views.py
+++ petstagram/pets/views.py
@@ -83,11 +83,6 @@ class CreatePetView(LoginRequiredMixin, BootStrapFormViewMixin, CreateView):
     success_url = reverse_lazy('list pets')
     template_name = 'pets/pet_create.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context = dict( backend_form = PetForm())
-        return context
-
     def form_valid(self, form):
         pet = form.save(commit=False)
         pet.user = self.request.user
